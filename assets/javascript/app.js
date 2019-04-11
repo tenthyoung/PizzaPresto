9a2b91ce39d26c44be6095e73f2f0ef2688f0abc
tenthyoung <tenthyoung@gmail.com>
2019-04-11 15:03:15 -0700

    added joke API

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+
+//========================================================//
+//Global Variables
+//========================================================//
+var jokesAPIResults = "";
+
+$(document).ready(function () {
+ 
+});
+//========================================================//
+//Functions
+//========================================================//
+
+//One the scoreboard page, the chef will say a random pizza joke
+//on the bottom
+function joke() {
+    $(document).on("click", '#button', function () {
+        var queryURL = "https://official-joke-api.appspot.com/random_joke";
+        
+        $.ajax({
+            url:queryURL,
+            method: 'GET'
+        }).then(function(response) {
+            console.log(response)
+            console.log(response.setup)
+            $('#setup').text(response.setup);
+            $('#punchline').text(response.punchline);
+            
+        });
+    });
+
+
+}
